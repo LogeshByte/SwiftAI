00a29ca87c1b1880061fdf9f7ef33ae3696d7c4d
Logeshwar <calogeshwar@gmail.com>
2025-07-17 16:37:01 +0530

    added bg and hero section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,35 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home.jsx'
-import Layout from './pages/Layout.jsx'
-import Dashboard from './pages/Dashboard'
-import WriteArticle from './pages/WriteArticle'
-import BlogTitle from './pages/BlogTitle'
-import GenerateImages from './pages/GenerateImages.jsx'
-import RemoveBackground from './pages/RemoveBackground.jsx'
-import ReviewResume from './pages/ReviewResume.jsx'
-import Community from './pages/Community.jsx'
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home.jsx";
+import Layout from "./pages/Layout.jsx";
+import Dashboard from "./pages/Dashboard";
+import WriteArticle from "./pages/WriteArticle";
+import BlogTitle from "./pages/BlogTitle";
+import GenerateImages from "./pages/GenerateImages.jsx";
+import RemoveBackground from "./pages/RemoveBackground.jsx";
+import ReviewResume from "./pages/ReviewResume.jsx";
+import Community from "./pages/Community.jsx";
+
 
 const App = () => {
   return (
     <div>
+      
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path="/" element={<Home />} />
 
-        <Route path='/ai' element={<Layout/>}>
-          <Route index element={<Dashboard/>}/>
-          <Route path='write-article' element={<WriteArticle/>}/>
-          <Route path='blog-titles' element={<BlogTitle/>}/>
-          <Route path='generate-images' element={<GenerateImages/>}/>
-          <Route path='remove-background' element={<RemoveBackground/>}/>
-          <Route path='remove-object' element={<RemoveBackground/>}/>
-          <Route path='review-resume' element={<ReviewResume/>}/>
-          <Route path='community' element={<Community/>}/>
+        <Route path="/ai" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="write-article" element={<WriteArticle />} />
+          <Route path="blog-titles" element={<BlogTitle />} />
+          <Route path="generate-images" element={<GenerateImages />} />
+          <Route path="remove-background" element={<RemoveBackground />} />
+          <Route path="remove-object" element={<RemoveBackground />} />
+          <Route path="review-resume" element={<ReviewResume />} />
+          <Route path="community" element={<Community />} />
         </Route>
-
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
